b5a8f865746b8721a05ac22f098bf8b6104f6013
narmijo-tj <toffee.jaguargmail.com>
2024-11-07 21:11:34 -0300

    Funcionando segun solicitado

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import ListaDeJuegos from '@/components/ListaDeJuegos.vue'
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text-center">Bienvenido a Tienda 32 Bits</h1>
+    <ListaDeJuegos/>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ListaDeJuegos.vue
@@ -0,0 +1,40 @@
+<script setup>
+    import { useJuegosStore } from '@/stores/juegosStore'
+    import { storeToRefs } from 'pinia'
+
+    const juegosStore = useJuegosStore()
+    const { juegos } = storeToRefs(juegosStore)
+    const { incrementar, decrementar } = juegosStore 
+</script>
+<template>
+    <div class="row">
+        <h2 class="mb-5 text-center">Lista de juegos</h2>
+        <div class="col-6 mx-auto">
+            <table class="table border border-secondary-subtle text-center">
+                <thead>
+                    <tr>
+                    <th>Codigo</th>
+                    <th>Nombre</th>
+                    <th>Stock</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="juego,i in juegos" :key="juego.codigo">
+                        <td>{{ juego.codigo }}</td>
+                        <td>{{ juego.nombre }}</td>
+                        <td>{{ juego.stock }}</td>
+                        <td>{{ juego.precio }}</td>
+                        <td>
+                            <button @click="incrementar(i)" class="btn btn-success fw-bold text-light mx-2">
+                            +
+                            </button>
+                            <button @click="decrementar(i)" class="btn btn-danger fw-bold text-light">-</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
